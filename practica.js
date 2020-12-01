9e9adaf04ea0a185ae883146fcb28fa974e40c62
nicolas <n.ebarja@gmail.com>
2020-12-01 10:41:58 -0300

    primer commit

---
+++ practica.js
@@ -0,0 +1,14 @@
+var nombreMascota = prompt("Ingrese el nombre de su mascota"); 
+var tipoMascota = prompt("Ingrese el tipo de su mascota"); 
+var razaMascota = prompt("Ingrese la raza de su mascota");
+
+var mascota = "Tengo una mascota que es un" + " " + tipoMascota + " " + "su nombre es" + " " + nombreMascota + " de la raza" + " " + razaMascota;
+document.write(mascota);
+
+alert("Hola!");
+
+var numero = prompt("Ingrese un numero");
+var resultado = numero * 6;
+
+alert(resultado);
+
